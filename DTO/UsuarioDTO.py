f40854f05ba17ca7c0ffe55a8e21129bf36d0a6b
aleberrot <berroteranbas@gmail.com>
2025-12-09 02:09:47 -0300

    Corrected UsuarioDTO and added __repr__ dunder method to print user

--- DTO/UsuarioDTO.py
+++ DTO/UsuarioDTO.py
@@ -1,16 +1,21 @@
 class UsuarioDTO:
 	"""docstring for Usuario"""
-	def __init__(self, id: int, nombre: str, email: str, password_hash: str):
-		self.id = id
+	def __init__(self, id_usuario: int, nombre: str, email: str, rol: str, password_hash: str):
+		self.id_usuario = id_usuario
 		self.nombre = nombre
 		self.email = email
+		self.rol = rol
 		self.password_hash = password_hash
 
+
+	def __repr__(self):
+		return f"Usuario(id={self.id_usuario}, nombre={self.nombre}, email={self.email}, rol={self.rol})"
 	def autenticar(email: str, password_hash: str) -> bool:
 		...
 
 	def registrar() -> bool:
 		...
+
 
 
 		
